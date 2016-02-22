f43d4c99915e3f5b5528a2e895511c2e484b5e86
Peter Carbonetto <pcarbonetto@ancestry.com>
2016-02-22 15:13:36 -0700

    Added steps to ancestry2ped.R for merging Ancestry genotype file with a .bim file. An important step here is to fix strand differences.

--- R/ancestry2ped.R
+++ R/ancestry2ped.R
@@ -1,4 +1,7 @@
 # TO DO: Explain here what this script does.
+#
+# Probably a combination of PLINK commands that could accomplish the
+# same thing.
 
 # SCRIPT PARAMETERS
 # -----------------
@@ -7,7 +10,7 @@
 # Ancestry.com; (2) bim.file, the list of SNPs to keep (set to NULL to
 # retain all the genotype data).
 geno.file <- "/DNAData2/peter/cehg16/private/peter_carbonetto_ancestry1.txt"
-bim.file  <- "/DNAData2/peter/cehg16/public/1kg_hgdp.bim"
+bim.file  <- "/DNAData2/peter/cehg16/public/panel/1kg_hgdp.bim"
 out.file  <- "peter_pcarbonetto1"
 fid       <- 0
 iid       <- "pc1"
@@ -21,28 +24,60 @@ geno        <- cbind(geno,data.frame(dist = 0))
 geno        <- geno[c("chr","id","dist","pos","A1","A2")]
 cat("Loaded genotype data at",nrow(geno),"SNPs.\n")
 
-# WRITE SNP DATA TO .map FILE
-# ---------------------------
-cat("Writing SNP data to .map file.\n")
-write.table(geno[c("chr","id","dist","pos")],paste0(out.file,".map"),
-            sep = " ",row.names = FALSE,col.names = FALSE,quote = FALSE)
-
-# WRITE GENOTYPE DATA TO .ped FILE
-# --------------------------------
-cat("Writing genotypes to .ped file.\n")
-write.table(cbind(data.frame(fid = fid,iid = iid,pid = 0,mid = 0,sex = 0,
-                             pheno = -9),geno[c("A1","A2")]),
-            paste0(out.file,".ped"),sep = " ",row.names = FALSE,
-            col.names = FALSE,quote = FALSE)
-
-stop()
-
-# READ SNP DATA
-# -------------
+# OPTIONALLY, SELECT SNPs IN .bim FILE
+# ------------------------------------
 if (!is.null(bim.file)) {
   cat("Reading SNP data from PLINK .bim file.\n")
   map          <- read.table(bim.file,stringsAsFactors = FALSE)
   names(map)   <- c("chr","id","dist","pos","A1","A2")
   map[,"dist"] <- 0
+  cat("Loaded map info for",nrow(map),"SNPs.\n")
+  
+  # Select SNPs that have the same identifer in the Ancestry file and
+  # in the PLINK .bim file.
+  rownames(geno) <- geno$id
+  rownames(map)  <- map$id
+  ids            <- intersect(geno$id,map$id)
+  geno           <- geno[ids,]
+  map            <- map[ids,]
+
+  # Next, I remove any SNPs that disagree on the chromosome. This is
+  # not a foolproof check, and there could remain some entries that
+  # refer to different SNPs after this check. But it should take care
+  # of most of errors that are likely to occur.
+  markers <- which(geno$chr == map$chr)
+  geno    <- geno[markers,]
+  map     <- map[markers,]
+
+  # Get the base-pair positions from the .bim file.
+  geno$pos       <- map$pos
+  rownames(geno) <- NULL
+  rownames(map)  <- NULL
+  
+  # Summarize the results of these processing steps.
+  cat("Aftering processing, we have genotypes at",nrow(geno),"SNPs.\n")
+  
+  # Fix SNPs that differ on the strand.
+  cat("Fixing strand differences to match .bim file.\n")
+  for (i in c("A1","A2")) {
+    geno[geno[[i]] == "A" & map$A1 != "A" & map$A2 != "A",i] <- "T"
+    geno[geno[[i]] == "T" & map$A1 != "T" & map$A2 != "T",i] <- "A"
+    geno[geno[[i]] == "G" & map$A1 != "G" & map$A2 != "G",i] <- "C"
+    geno[geno[[i]] == "C" & map$A1 != "C" & map$A2 != "C",i] <- "G"
+  }
 }
 
+
+# WRITE SNP DATA TO .map FILE
+# ---------------------------
+cat("Writing SNP data to PLINK .map file.\n")
+write.table(geno[c("chr","id","dist","pos")],paste0(out.file,".map"),
+            sep = " ",row.names = FALSE,col.names = FALSE,quote = FALSE)
+
+# WRITE GENOTYPE DATA TO .ped FILE
+# --------------------------------
+cat("Writing genotypes to PLINK .ped file.\n")
+write.table(cbind(data.frame(fid = fid,iid = iid,pid = 0,mid = 0,sex = 0,
+                             pheno = -9),geno[c("A1","A2")]),
+            paste0(out.file,".ped"),sep = " ",row.names = FALSE,
+            col.names = FALSE,quote = FALSE)
