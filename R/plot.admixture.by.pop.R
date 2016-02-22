ae4859d3d89a67def329fa85c15c23ced4bf822c
Peter Carbonetto <pcarbonetto@ancestry.com>
2016-02-22 14:52:13 -0800

    Removed *.P files.

---
+++ R/plot.admixture.by.pop.R
@@ -0,0 +1,18 @@
+# TO DO: Explain here what this script does.
+
+# SCRIPT PARAMETERS
+# -----------------
+fam.file    <- "../data/panel/1kg_hgdp.fam"
+labels.file <- "../data/panel/1kg_hgdp.lab"
+admix.file  <- "../data/admixture/1kg_hgdp.7.Q"
+  
+# LOAD SAMPLE DATA FROM .fam FILE
+# -------------------------------
+cat("")
+panel <-
+
+# 
+
+# LOAD ADMIXTURE ESTIMATES
+# ------------------------
+admix <- as.matrix(read.table(admix.file,sep = " "))
