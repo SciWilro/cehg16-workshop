029c1a6696c939fc66d4c1c02637b3fa0321ee6a
Peter Carbonetto <pcarbonetto@ancestry.com>
2016-02-22 16:04:05 -0800

    Wrote first draft of script to summarize ADMIXTURE results by ancestral population.

--- R/plot.admixture.by.pop.R
+++ R/plot.admixture.by.pop.R
@@ -1,18 +1,60 @@
-# TO DO: Explain here what this script does.
+# TO DO: Explain here what this script does, and how to use it.
+library(ggplot2)
 
 # SCRIPT PARAMETERS
 # -----------------
-fam.file    <- "../data/panel/1kg_hgdp.fam"
+k           <- 7
+fam.file    <- "../data/panel/1kg_hgdp_train.fam"
 labels.file <- "../data/panel/1kg_hgdp.lab"
-admix.file  <- "../data/admixture/1kg_hgdp.7.Q"
-  
+admix.file  <- "../data/admixture/1kg_hgdp_train.7.Q"
+which.label <- "label2"
+int         <- c(0.05,0.95)
+
 # LOAD SAMPLE DATA FROM .fam FILE
 # -------------------------------
-cat("")
-panel <-
+cat("Loading sample info from PLINK .fam file.\n")
+panel        <- read.table(fam.file,stringsAsFactors = FALSE)
+names(panel) <- c("fid","iid","pid","mid","sex","pheno")
 
-# 
+# LOADING GROUP LABELS
+# --------------------
+cat("Loading group labels assigned to samples.\n")
+labels           <- read.table(labels.file,as.is = "id",header = TRUE)
+rownames(labels) <- labels$id
+panel <- cbind(panel,data.frame(label = labels[panel$iid,which.label]))
+rm(labels)
 
 # LOAD ADMIXTURE ESTIMATES
 # ------------------------
-admix <- as.matrix(read.table(admix.file,sep = " "))
+cat("Loading admixture estimates.\n")
+admix           <- as.matrix(read.table(admix.file))
+rownames(admix) <- panel$iid
+colnames(admix) <- paste0("K",1:ncol(admix))
+
+# SUMMARIZE CONTRIBUTIONS FROM SELECTED ANCESTRAL POPULATION
+# ----------------------------------------------------------
+dev.new(height = 8,width = 5)
+admix.stats <-
+  data.frame(n = as.vector(table(panel$label)),
+             x = tapply(admix[,k],panel$label,mean),
+             a = tapply(admix[,k],panel$label,function(x) quantile(x,int[1])),
+             b = tapply(admix[,k],panel$label,function(x) quantile(x,int[2])))
+admix.stats <- admix.stats[order(admix.stats$x),]
+n           <- nrow(admix.stats)
+print(ggplot(cbind(admix.stats,y = 1:n),aes(x = x,y = y)) +
+      geom_point(col = "dodgerblue",cex = 1.4) +
+      geom_errorbarh(aes(xmin = a,xmax = b),col = "dodgerblue",height = 0) +
+      theme_minimal() +
+      scale_x_continuous(limits = c(-0.05,1.05),breaks = c(0,0.5,1)) +
+      scale_y_continuous(breaks = 1:n,
+                         labels = rownames(admix.stats)) +
+      theme(panel.grid.major  = element_blank(),
+            panel.grid.minor  = element_blank(),
+            axis.text.x       = element_text(size = 9),
+            axis.text.y       = element_text(size = 9),
+            axis.title        = element_text(size = 10),
+            plot.title        = element_text(size = 10)) +
+      labs(title = paste("ancestral population k =",k),
+           x = "admixture proportion",y = ""))
+
+
